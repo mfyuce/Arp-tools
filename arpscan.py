510dc279789c02350b0c1f19101e6fd1483db9ed
mehmet.yuce <mehmet.yuce@logosgt.com.tr>
2018-06-27 11:17:15 +0300

    elevated run

--- arpscan.py
+++ arpscan.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+from elevate import elevate
+import subprocess
+import os
+
 
 def missing_library(string):
     raise Exception("\nMissing %s Library\n" % (string))
@@ -35,9 +39,9 @@ def signal_exit(signal, frame):
 
 
 def usage():
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 4:
         print("\nUsage:")
-        print("\tpython arp-scan.py -l <IPs>")
+        print("\tpython arp-scan.py -l -s/-v <IPs>")
         print("\t<ips> is a single ip, range , or list of IPs (separated by \",\")\n")
         sys.exit(1)
 
@@ -75,36 +79,46 @@ def scan_devices(ip):
 
 def check_root():
     if not os.geteuid() == 0:
-        raise Exception("Run as root.")
+        elevate()
+        # raise Exception("Run as root.")
 
 
-def get_mac_from_ip(ip, subset):
+def get_mac_from_ip(ip, subset, silent):
     check_root()
     parameters = {subset: ip}
 
     if "/" in parameters[subset]:
-        print("[*] Scanning subnet %s" % (parameters[subset]))
-        print("\n%-20s%s" % ("IP", "MAC"))
+        if not silent:
+            print("[*] Scanning subnet %s" % (parameters[subset]))
+            print("\n%-20s%s" % ("IP", "MAC"))
         for s in decode_netmask(parameters[subset]):
             for pair in s:
                 print("%-20s%s" % (pair[1].psrc, pair[1].hwsrc))
     elif "," in parameters[subset]:
-        print("[*] Scanning list of IPs %s" % (parameters[subset]))
-        print("\n%-20s%s" % ("IP", "MAC"))
+        if not silent:
+            print("[*] Scanning list of IPs %s" % (parameters[subset]))
+            print("\n%-20s%s" % ("IP", "MAC"))
         for s in decode_enumeration(parameters[subset]):
             for pair in s:
                 print("%-20s%s" % (pair[1].psrc, pair[1].hwsrc))
     else:
-        print("[*] Scanning Single IP %s" % (parameters[subset]))
-        print("\n%-20s%s" % ("IP", "MAC"))
+        if not silent:
+            print("[*] Scanning Single IP %s" % (parameters[subset]))
+            print("\n%s" % "MAC")
         for pair in scan_devices(parameters[subset]):
-            print("%-20s%s" % (pair[1].psrc, pair[1].hwsrc))
+            print("%s" % pair[1].hwsrc)
+
+
+def cmd_get_mac_from_ip(ip, is_silent):
+    return subprocess.check_output(["python", os.path.abspath(__file__), "-l", "-s" if is_silent else "-v", ip]).decode()
 
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
     usage()
-
-    print("\n[*] Scanning for active IPs")
-    get_mac_from_ip(sys.argv[2], sys.argv[1] == "-l")
-    print("")
+    silent = sys.argv[2] == "-s"
+    if not silent:
+        print("\n[*] Scanning for active IPs")
+    get_mac_from_ip(sys.argv[3], sys.argv[1] == "-l", silent)
+    if not silent:
+        print("")
